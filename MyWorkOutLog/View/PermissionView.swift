2581036b7c0f588aa41f22163c7e319b2e6fe17f
Jongchan Ko <kjc@hanryubank.com>
2024-01-08 17:39:07 +0900

    [Edit] Prototype 완성 Todo: 추가기능 디버깅 구상할 것

---
+++ MyWorkOutLog/View/PermissionView.swift
@@ -0,0 +1,18 @@
+//
+//  PermissionView.swift
+//  MyWorkOutLog
+//
+//  Created by 고종찬 on 2024/01/08.
+//
+
+import SwiftUI
+
+struct PermissionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PermissionView()
+}
