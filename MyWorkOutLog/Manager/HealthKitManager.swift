19d9c4ceb043f8f862826d372c327da1e90d2aab
Jongchan Ko <kjc@hanryubank.com>
2024-01-10 18:01:37 +0900

    [Edit] HealthKit 연동 완료 어떻게 사용할지 고려해볼 것 설정 탭 추가 필요

---
+++ MyWorkOutLog/Manager/HealthKitManager.swift
@@ -0,0 +1,50 @@
+//
+//  File.swift
+//  MyWorkOutLog
+//
+//  Created by 고종찬 on 2024/01/10.
+//
+
+import Foundation
+import HealthKit
+
+class ExerciseManager {
+    let healthStore = HKHealthStore()
+
+    // 권한 요청
+    func requestAuthorization(completion: @escaping (Bool, Error?) -> Void) {
+        guard HKHealthStore.isHealthDataAvailable() else {
+            completion(false, HealthError.dataNotAvailable)
+            return
+        }
+
+        let typesToRead: Set<HKObjectType> = [
+            HKObjectType.workoutType(),
+            HKObjectType.quantityType(forIdentifier: .activeEnergyBurned)!,
+            HKObjectType.quantityType(forIdentifier: .appleExerciseTime)!
+        ]
+
+        healthStore.requestAuthorization(toShare: nil, read: typesToRead) { success, error in
+            completion(success, error)
+        }
+    }
+
+    // 운동 기록 가져오기
+    func fetchWorkouts(completion: @escaping ([HKWorkout]?, Error?) -> Void) {
+        let workoutPredicate = HKQuery.predicateForWorkouts(with: .any)
+        let sortDescriptor = NSSortDescriptor(key: HKSampleSortIdentifierEndDate, ascending: false)
+        let query = HKSampleQuery(sampleType: .workoutType(), predicate: workoutPredicate, limit: HKObjectQueryNoLimit, sortDescriptors: [sortDescriptor]) { _, samples, error in
+            guard let workouts = samples as? [HKWorkout], error == nil else {
+                completion(nil, error)
+                return
+            }
+            completion(workouts, nil)
+        }
+        healthStore.execute(query)
+    }
+}
+
+// 에러 처리를 위한 커스텀 에러 유형
+enum HealthError: Error {
+    case dataNotAvailable
+}
